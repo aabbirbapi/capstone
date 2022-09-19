c6bc4eba27836197c772d5b6443efd8a578bf589
aabbirbapi <110724190+aabbirbapi@users.noreply.github.com>
2022-09-19 15:23:06 +0530

    Update App.js

--- product-webapp/web-app/src/App.js
+++ product-webapp/web-app/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+/* import React from 'react'
 
-import AppRoutes from './routes/AppRoutes'
+ import AppRoutes from './routes/AppRoutes'
 
-const App = () => {
+ const App = () => {
   return (
     <AppRoutes/>
   )
 }
 
 export default App
+*/
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js';
+import {routes} from './routes'
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+function App() {
+  return (
+    <div >
+       <div >
+        <BrowserRouter>
+          <Routes>
+              {routes.map((route , id) =>{
+                    return <Route
+                    key={id}
+                    path={route.path}
+                    name={route.name}
+                    element={<route.component />}
+                />
+              })}
+          </Routes>
+        </BrowserRouter>
+    </div>
+    </div>
+  );
+}
+export default App;
